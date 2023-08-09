c63f500ccd91c63b66dde507e365a28e920d53a6
= <eric.indiran@gmail.com>
2023-08-08 22:28:52 -0700

    added bokking and menu models

--- LittleLemon/Restaurant/admin.py
+++ LittleLemon/Restaurant/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Booking, Menu
 
 # Register your models here.
+# Permits access to manage your models in the admin site by logging in with a superuser account.
+admin.site.register(Booking)
+admin.site.register(Menu)
