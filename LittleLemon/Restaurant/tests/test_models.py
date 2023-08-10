14bb4165f5425af89ce88bc3b26a655a6d0e0d3d
= <eric.indiran@gmail.com>
2023-08-10 16:15:59 -0700

    fixed comment typo

--- LittleLemon/Restaurant/tests/test_models.py
+++ LittleLemon/Restaurant/tests/test_models.py
@@ -6,7 +6,7 @@ class MenuViewTest(TestCase):
     
     @classmethod
     def setUpTestData(cls) -> None:
-        #setup dtata for all tests cases
+        #setup data for all tests cases
         cls.item1 = Menu.objects.create(Title="Ice Cream", Price=3, Inventory=100)
         cls.item2 = Menu.objects.create(Title="Cheesecake", Price=12, Inventory=15)
     
